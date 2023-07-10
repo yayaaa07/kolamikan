2ed00ff716050a400da5bfab46cc406cd201dabf
yayaaa07 <yulianaputri326@gmail.com>
2023-07-10 22:49:28 +0700

    kondisi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,7 @@
 <?php
 $con = mysqli_connect("localhost","root","","kolamikan");
- 
+// $con = mysqli_connect("localhost","root","12345","kolamikan");
+
 // Melihat koneksi error atau tidak
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
