a4feb505234a1b227eed8efbbb98d211f64056c8
yayaaa07 <yulianaputri326@gmail.com>
2023-07-12 00:33:53 +0700

    ubah keterangannya

--- ketketinggian.php
+++ ketketinggian.php
@@ -4,9 +4,9 @@ include "koneksi.php";
 $sql = mysqli_query($con, "SELECT ketinggian FROM sensor order by id DESC");
 $data=mysqli_fetch_array($sql);
 $ketinggian=$data["ketinggian"];
-if($ketinggian > 10 ){
-    $ket = "Aman";
-}else{
+if($ketinggian > 15 ){
     $ket = "Tidak Aman";
+}else{
+    $ket = "Aman";
 }
 echo  $ket;
