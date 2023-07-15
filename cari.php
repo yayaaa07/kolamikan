71999ad446dbdeabd02d3b4ad2c454aabcca755d
yayaaa07 <yulianaputri326@gmail.com>
2023-07-15 16:27:34 +0700

    ubah nama file

--- cari.php
+++ cari.php
@@ -5,7 +5,7 @@ include "koneksi.php";
 if (isset($_POST['cari'])) {
 
 $cari = $_POST['cari'];
-$sql = mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM datasensor WHERE LEFT(waktu, 10) = '$cari' GROUP BY LEFT(waktu, 10)");
+$sql = mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor WHERE LEFT(waktu, 10) = '$cari' GROUP BY LEFT(waktu, 10)");
 }
 
 
