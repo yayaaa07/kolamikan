ba26d92908e9165ecdac27e842dc39e24a1b66b6
yayaaa07 <yulianaputri326@gmail.com>
2023-07-30 11:34:35 +0700

    ubah data

--- insertdata.php
+++ insertdata.php
@@ -4,14 +4,14 @@
  include "koneksi.php";
 
  // Prepare the SQL statement
- $var1 = $_GET['nominal'];
- $var2 = $_GET['banyak'];
+ $var1 = $_GET['suhu'];
+ $var2 = $_GET['tinggi'];
  $var3 = $_GET['cuaca'];
  $var4 = date("Y-m-d H:i:s");
      
- $result = mysqli_query ($con,"INSERT INTO sensor (suhu,ketinggian,cuaca,waktu) VALUES ('$var1','$var2','$var3','$var)");
+ $result = mysqli_query ($con,"INSERT INTO sensor (suhu,ketinggian,cuaca,waktu) VALUES ('$var1','$var2','$var3','$var4')");
  if (!$result) 
      {
-         die ('Invalid query: '.mysqli_error($conn));
+         die ('Invalid query: '.mysqli_error($con));
      }  
 ?>
