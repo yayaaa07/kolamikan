7072a067a580a00f95c1485acd83065d19bc42f5
yayaaa07 <yulianaputri326@gmail.com>
2023-07-30 11:16:56 +0700

    ubah isinya

--- data.php
+++ data.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <title>Data Monitoring Kolam Ikan</title>
 </head>
@@ -18,11 +18,11 @@
                 <h1>Data Monitoring Kolam Ikan</h1>
                 <div class="row justify-content-center">
                     <form action="data.php" method="get" class="form-inline">
-                    <div class="from-group">
+                    <div class="form-group">
                         <input type="date" name="cari" id="tanggal">
                     </div>
                     <div class="form-group col-sm-2">
-                    <button type="submit"  class="btn btn-primary mb-2">Cari</button>
+                    <button type="submit" class="btn btn-primary mb-2">Cari</button>
                     </div>
                     </form>
                 </div>
@@ -31,11 +31,11 @@
                     <table class="table">
                         <thead class="thead-dark">
                             <tr>
-                                <td scope="col">No</td>
-                                <td scope="col">Suhu</td>
-                                <td scope="col">Ketinggian</td>
-                                <td scope="col">Cuaca</td>
-                                <td scope="col">Waktu</td>
+                                <th scope="col">No</th>
+                                <th scope="col">Suhu</th>
+                                <th scope="col">Ketinggian</th>
+                                <th scope="col">Cuaca</th>
+                                <th scope="col">Waktu</th>
                             </tr>
                         </thead>
                         <tbody> 
@@ -43,41 +43,34 @@
                             <?php 
                                 include 'koneksi.php';
                                 $batas = 10;
-                                $halaman =isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
-                                $hal_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;
-                                $previous = $halaman-1;
-                                $next = $halaman +1;
-                                $data =  mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor GROUP BY day(waktu)");
-                                $jumlah_data = mysqli_num_rows($data);
-                                $total_hal = ceil($jumlah_data / $batas);
-                                $no = $hal_awal +1;
+                                $halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
+                                $hal_awal = ($halaman > 1) ? ($halaman * $batas) - $batas : 0;
+                                $previous = $halaman - 1;
+                                $next = $halaman + 1;
+
+                                // Ubah query berikut agar data sesuai dengan tanggal yang dipilih
                                 if ($_GET['cari']) {
                                     $cari = $_GET['cari'];
-                                $sql =  mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor WHERE LEFT(waktu, 10) = '$cari' GROUP BY day(waktu) LIMIT $hal_awal, $batas");
-
-                                }else if (!$_GET['cari'] == null){
-                                    $sql =  mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor WHERE day(waktu)  GROUP BY day(waktu)  ORDER BY day(waktu) DESC LIMIT $hal_awal, $batas");
-                                }else{
-                                    $sql =  mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor WHERE day(waktu)  GROUP BY day(waktu)  ORDER BY day(waktu) DESC LIMIT $hal_awal, $batas");
+                                    $sql = mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor WHERE LEFT(waktu, 10) = '$cari' GROUP BY LEFT(waktu, 10) ORDER BY waktu DESC LIMIT $hal_awal, $batas");
+                                } else {
+                                    $sql = mysqli_query($con, "SELECT avg(format(suhu,2)) as suhu, avg(format(ketinggian,2)) as ketinggian, if(count(cuaca = 'hujan') > count(cuaca = 'tidak hujan'), 'Hujan', 'Tidak Hujan') as cuaca, LEFT(waktu, 10) as waktu FROM sensor GROUP BY LEFT(waktu, 10) ORDER BY waktu DESC LIMIT $hal_awal, $batas");
                                 }
-                               
+
                                 $result = array();
-                                while($row = $sql -> fetch_array()){
+                                while ($row = $sql->fetch_assoc()) {
                                     $suhu = number_format($row["suhu"], 2);
                                     $ketinggian = number_format($row["ketinggian"], 2);
-                                    $waktu = $row["waktu"];
                                     $result[] = $row;
                                 }   
                                 
-                                
-                                
+                                $no = $hal_awal + 1;
                                 foreach ($result as $res) {
                                 
                             ?>
                             <tr>
-                                <td><?php echo $no++ ?></td>
+                                <td><?php echo $no++; ?></td>
                                 <td><?php echo $suhu. " ℃"; ?></td>
-                                <td><?php echo $ketinggian." Cm" ; ?></td>
+                                <td><?php echo $ketinggian." Cm"; ?></td>
                                 <td><?php echo $res['cuaca']; ?></td>
                                 <td><?php echo $res['waktu']; ?></td>
                             </tr>
