ebf5676c93fe146a7b931dae335c81b429bd3067
yayaaa07 <yulianaputri326@gmail.com>
2023-08-27 23:33:17 +0700

    ganti span nama logo

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <a class="brand-link">
         <img src="dist/img/logo.png" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Pemantauan Kolam Ikan</span>
+        <span class="brand-text font-weight-light" style="font-size: 12px;">Pemantauan Kolam Ikan</span>
       </a>
 
       <nav class="mt-2">
