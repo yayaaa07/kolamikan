63ef0886e350cf2505fc8da40d92d7231a78f3e3
yayaaa07 <yulianaputri326@gmail.com>
2023-08-30 09:56:45 +0700

    ketinggian

--- cekketinggian.php
+++ cekketinggian.php
@@ -1,29 +1,34 @@
 <?php
 include "koneksi.php";
 
-$sql = mysqli_query($con, "SELECT avg(suhu) as suhu, avg(ketinggian) as ketinggian, avg(ph) as ph, LEFT(waktu, 10) as waktu FROM sensor GROUP BY day(waktu) ORDER BY waktu DESC LIMIT 5");
+$sql = mysqli_query($con, "SELECT ketinggian FROM sensor ORDER BY id DESC");
 $data = mysqli_fetch_array($sql);
 $ketinggian = number_format($data["ketinggian"], 2); 
 
-$alert = null;
+$alert = '';
 if ($ketinggian > 150) {
-    $alert = "Buka Penutup Air Agar Air Berkurang dan Tidak Penuh";
-} elseif ($ketinggian < 50) {
-    $alert = "Ketinggian Air Rendah, Tambahkan Air Agar Tidak Kering";
-}
-
-echo $ketinggian;
-
-if ($alert) {
-    $alertScript = "<script>
+    $alert = "<script>
         swal({  
-            title: '$alert',
+            title: 'Buka Penutup Air Agar Air Berkurang dan Tidak Penuh',
             type: 'info',
             showCancelButton: true,
-            reverseButtons: true,  
-            confirmButtonColor: '#ff0055'
+            confirmButtonColor: '#ff0055',
+            reverseButtons: true,
+            focusConfirm: false,
+            focusCancel: true
         });
     </script>";
-    echo $alertScript;
+} elseif ($ketinggian < 50) {
+    $alert = "<script>
+        swal({  
+            title: 'Ketinggian Air Rendah, Tambahkan Air Agar Tidak Kering',
+            type: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#ff0055',
+            reverseButtons: true,
+        });
+    </script>";
 }
+
+echo $ketinggian . $alert;
 ?>
