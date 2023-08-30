97ed10ff5678a833da8405f32dfb8c061f943611
yayaaa07 <yulianaputri326@gmail.com>
2023-08-30 10:04:42 +0700

    save

--- cekph.php
+++ cekph.php
@@ -1,7 +1,7 @@
 <?php
 include "koneksi.php";
 
-$sql = mysqli_query($con, "SELECT avg(suhu) as suhu, avg(ketinggian) as ketinggian, avg(ph) as ph, LEFT(waktu, 10) as waktu FROM sensor GROUP BY day(waktu) ORDER BY waktu DESC LIMIT 5");
+$sql = mysqli_query($con, "SELECT suhu FROM sensor ORDER BY id DESC");
 $data = mysqli_fetch_array($sql);
 $ph = number_format($data["ph"], 2); 
 
