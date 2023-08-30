018ed99211c03d69b1607f2e74a819567db96abe
yayaaa07 <yulianaputri326@gmail.com>
2023-08-30 10:53:03 +0700

    INIII

--- cekph.php
+++ cekph.php
@@ -1,29 +1,32 @@
 <?php
 include "koneksi.php";
 
-$sql = mysqli_query($con, "SELECT avg(suhu) as suhu, avg(ketinggian) as ketinggian, avg(ph) as ph, LEFT(waktu, 10) as waktu FROM sensor GROUP BY day(waktu) ORDER BY waktu DESC LIMIT 5");
+$sql = mysqli_query($con, "SELECT ph FROM sensor ORDER BY id DESC");
 $data = mysqli_fetch_array($sql);
-$ph = number_format($data["ph"], 2); 
+$ph = $data["ph"];
 
 $alert = null;
 if ($ph < 5) {
-    $alert = "pH Air Terlalu Rendah, Lakukan Penyesuaian pH";
-} elseif ($ph > 8) {
-    $alert = "pH Air Terlalu Tinggi, Lakukan Penyesuaian pH";
-}
-
-echo $ph;
-
-if ($alert) {
-    $alertScript = "<script>
+    $alert = "<script>
         swal({  
-            title: '$alert',
+            title: 'pH Air Terlalu Rendah, Lakukan Penyesuaian pH',
             type: 'info',
             showCancelButton: true,
             reverseButtons: true,  
             confirmButtonColor: '#ff0055'
         });
     </script>";
-    echo $alertScript;
+} elseif ($ph > 8) {
+    $alert = "<script>
+        swal({  
+            title: 'pH Air Terlalu Tinggi, Lakukan Penyesuaian pH',
+            type: 'info',
+            showCancelButton: true,
+            reverseButtons: true,  
+            confirmButtonColor: '#ff0055'
+        });
+    </script>";
 }
+
+echo $ph, $alert;
 ?>
